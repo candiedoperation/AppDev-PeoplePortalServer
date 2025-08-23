a69c17e5859d1da22fd55344f5a01f02b758a8ee
Sanya Gupta <sanyagup07@gmail.com>
2025-08-22 23:00:44 -0400

    add SlackClient and Authentik user email endpoint (/api/org/emails/{userId})
    Added a SlackClient wrapper around @slack/web-api with getUserIdByEmail, checkIfUserExists, ensureUserInUserGroup, and ensureUserInChannel.
    
    Exposed GET /api/org/emails/{userId} in OrgController to return a user’s email via Authentik.
    
    Implemented AuthentikClient.getUserEmail(userId) to GET /api/v3/core/users/{userId}/ and return res.data.email.

--- src/clients/AuthentikClient/index.ts
+++ src/clients/AuthentikClient/index.ts
@@ -40,6 +40,22 @@ export class AuthentikClient {
         if (!this.AUTHENTIK_TOKEN)
             throw new Error("Authentik Token is Invalid!")
     }
+
+    public getUserEmail = async (userId: string): Promise<string> => {
+        var RequestConfig: any = {
+            ...this.AxiosBaseConfig,
+            method: 'get',
+            url: `/api/v3/core/users/${userId}/`
+        }
+
+        try {
+            const res = await axios.request(RequestConfig)
+            return res.data.email
+        } catch (e) {
+            log.error(AuthentikClient.TAG, "Get User Email Request Failed with Error: ", e)
+            throw new AuthentikClientError("Get User Email Request Failed")
+        }
+    }
     
     public getUserList = async (options: GetUserListOptions): Promise<GetUserListResponse> => {
         var RequestConfig: any = {
